5feb1e64925c13597466fb61cb41b5f52312737f
Chudleyj <chudleyjustin@gmail.com>
2018-01-29 19:15:02 -0500

    Added comments to make code clearer

--- CryptoPricing.js
+++ CryptoPricing.js
@@ -1,13 +1,16 @@
-var CoinMarketCap = require ("node-coinmarketcap");
-var Sequelize = require("sequelize");
+var CoinMarketCap = require ("node-coinmarketcap"); //CoinMarketCap API
+var Sequelize = require("sequelize"); //Sequelize API
 
-var connection = new Sequelize('database', 'justin_chudley', 'password', {
+//Change database, justin_chudley, and password to your personal cases for each.
+//Do the same for localhost.
+var connection = new Sequelize('database', 'justin_chudley', 'password', { //Connect to DB
   host: 'localhost',
   dialect: 'postgres',
   logging: false,
   operatorsAliases: false
 });
 
+//Create Table and rows if not created yet
 var Cryptos = connection.define('cryptos', {
   Bitcoin: Sequelize.FLOAT,
   Ripple: Sequelize.FLOAT,
@@ -16,7 +19,7 @@ var Cryptos = connection.define('cryptos', {
 
 var options = {
   events: true,
-  refresh: 1,
+  refresh: 1, //Refresh data (coinmarketcap.on) every 1 second
   convert: "USD"
 }
 
@@ -25,23 +28,26 @@ var BTC;
 var RIP;
 var STEL;
 
+//Get BTC price
 coinmarketcap.on("BTC", (coin) => {
   BTC = coin.price_usd;
 });
 
+//Get ripple price
 coinmarketcap.on("ripple", (coin) => {
   RIP = coin.price_usd;
 //  console.log(coin)
 });
 
+//Get Stellar price, output all 3 to console
 coinmarketcap.on("stellar", (coin) => {
   STEL = coin.price_usd;
-//clears console  console.log('\033[2J');
   console.log("\n ................................................\n",
   "Stellar:", STEL, " BTC:", BTC, " Ripple:", RIP);
-  updateDB();
+  updateDB(); //Send prices to database update function
 });
 
+//Send prices to database
 function updateDB(){
   connection.sync().then(function (){
     Cryptos.create({
--- Graph.py
+++ Graph.py
@@ -1,25 +1,26 @@
-import matplotlib.pylab as plt
-import matplotlib.animation as animation
-import numpy as np
-import psycopg2 as p
+import matplotlib.pylab as plt #Graphing
+import matplotlib.animation as animation #Self updating Graphs
+import psycopg2 as p #Postgres SQL
 
 fig = plt.figure()
-st = fig.suptitle("Crypto Currency Prices", fontsize = "large")
+st = fig.suptitle("Crypto Currency Prices", fontsize = "large") #Graph window title
 
-ax1 = fig.add_subplot(212)
-ax2 = fig.add_subplot(221)
-ax3 = fig.add_subplot(222)
+ax1 = fig.add_subplot(212) #Bitcoin
+ax2 = fig.add_subplot(221) #Ripple
+ax3 = fig.add_subplot(222) #Stellar
 
+#Self Updating graph loop
 def animate(i):
 
+    #Change dbname, user, host, and password to your personal answers for each :)
     try:
         conn = p.connect("dbname= 'database' user= 'justin_chudley' host= 'localhost' password= 'password'")
     except:
         print "Database connection failed!"
 
     cur = conn.cursor()
-    cur.execute("""SELECT * from cryptos """)
-    rows = cur.fetchall()
+    cur.execute("""SELECT * from cryptos """) #Get ALL data from database
+    rows = cur.fetchall() #Save ALL data from database to rows
 
     ID = [] #ID value in database, new ID is created every second, therefore this can track seconds elapsed
     BTC = [] #Bitcoin
@@ -28,34 +29,43 @@ def animate(i):
 
 
     for x in range(0, len(rows)):
-        ID.append(rows[x][0])
-        BTC.append(rows[x][1])
-        RIP.append(rows[x][2])
-        STEL.append(rows[x][3])
+        ID.append(rows[x][0]) #Get ID column data isolated
+        BTC.append(rows[x][1]) #Get Bitcoin column data isolated
+        RIP.append(rows[x][2]) #Get Ripple column data isolated
+        STEL.append(rows[x][3]) #Get Stellar column data isolated
 
-
+    #Must clear old graph data every second for new updated graphs
     ax1.clear()
     ax2.clear()
     ax3.clear()
 
+    #Titels of each subgraph
     ax1.set_title('Bitcoin')
     ax2.set_title('Ripple')
     ax3.set_title('Stellar')
 
+    #Labels for graph axis
     ax1.set_xlabel('Seconds Elapsed')
     ax1.set_ylabel('Price (USD)')
 
+    #Labels for graph axis
     ax2.set_xlabel('Seconds Elapsed')
     ax2.set_ylabel('Price (USD)')
 
+    #Labels for graph axis
     ax3.set_xlabel('Seconds Elapsed')
     ax3.set_ylabel('Price (USD)')
 
+    #Plot each subgraph
     ax1.plot(ID, BTC)
     ax2.plot(ID,RIP)
     ax3.plot(ID,STEL)
 
-
+#Fit to window without overlaps
 fig.set_tight_layout(True)
+
+#Update graph with animate function, inveral = 1000 means every 1 second
 ani = animation.FuncAnimation(fig, animate, interval = 1000)
+
+#Draw the graph to the window
 plt.show()
